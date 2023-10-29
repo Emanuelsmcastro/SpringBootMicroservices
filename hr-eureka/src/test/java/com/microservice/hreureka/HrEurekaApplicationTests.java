19021b6c6836b8820587532b5fabafe27173ddbe
Emanuel Castro <emanuelsmcastro3@gmail.com>
2023-10-29 13:11:17 -0300

    add eureka server

---
+++ hr-eureka/src/test/java/com/microservice/hreureka/HrEurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microservice.hreureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrEurekaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
