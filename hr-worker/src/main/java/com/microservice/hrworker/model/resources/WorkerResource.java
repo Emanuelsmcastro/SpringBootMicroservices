a2bdf2f19e176ece83376bfad5b7278f9b750ac2
Emanuel Castro <emanuelsmcastro3@gmail.com>
2023-10-29 12:50:29 -0300

    fix ambiguous methods - hr-worker

--- hr-worker/src/main/java/com/microservice/hrworker/model/resources/WorkerResource.java
+++ hr-worker/src/main/java/com/microservice/hrworker/model/resources/WorkerResource.java
@@ -6,6 +6,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.microservice.hrworker.model.entities.Worker;
@@ -30,8 +31,8 @@ public class WorkerResource {
 		return ResponseEntity.ok().body(service.findById(id));
 	}
 	
-	@GetMapping(value = "/{name}")
-	public ResponseEntity<Worker> findById(@PathVariable String name){
+	@GetMapping(value = "/search")
+	public ResponseEntity<Worker> findByName(@RequestParam String name){
 		return ResponseEntity.ok().body(service.findByName(name));
 	}
 }
