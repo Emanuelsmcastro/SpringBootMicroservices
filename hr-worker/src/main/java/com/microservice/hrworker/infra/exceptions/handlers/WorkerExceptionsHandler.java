bab068af7fc474bfc98f843661c32bd0a8681f12
Emanuel Castro <emanuelsmcastro3@gmail.com>
2023-10-29 13:02:13 -0300

    create exception handler - hr-worker

---
+++ hr-worker/src/main/java/com/microservice/hrworker/infra/exceptions/handlers/WorkerExceptionsHandler.java
@@ -0,0 +1,25 @@
+package com.microservice.hrworker.infra.exceptions.handlers;
+
+import java.time.Instant;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import com.microservice.hrworker.infra.exceptions.ExceptionFormatter;
+import com.microservice.hrworker.infra.exceptions.WorkerNotFoundException;
+
+@ControllerAdvice
+public class WorkerExceptionsHandler extends RuntimeException {
+
+	private static final long serialVersionUID = 1L;
+	
+	@ExceptionHandler(WorkerNotFoundException.class)
+	public ResponseEntity<?> workerNotFoundHandler(WorkerNotFoundException e){
+		HttpStatus status = HttpStatus.NOT_FOUND;
+		ExceptionFormatter formatter = new ExceptionFormatter(status.value(), e.getMessage(), Instant.now());
+		return new ResponseEntity<>(formatter, status);
+	}
+
+}
