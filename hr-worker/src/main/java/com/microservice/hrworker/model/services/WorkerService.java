80701209ce11a32db18f90ca33c893105ad6e43c
Emanuel Castro <emanuelsmcastro3@gmail.com>
2023-10-29 12:40:17 -0300

    create worker service - hr-worker

---
+++ hr-worker/src/main/java/com/microservice/hrworker/model/services/WorkerService.java
@@ -0,0 +1,30 @@
+package com.microservice.hrworker.model.services;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.microservice.hrworker.model.entities.Worker;
+import com.microservice.hrworker.model.repositories.WorkerRepository;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class WorkerService {
+	
+	private final WorkerRepository rep;
+	
+	public Worker findById(Long id) {
+		return rep.findById(id).get();
+	}
+	
+	public Worker findByName(String name) {
+		return rep.findByName(name);
+	}
+	
+	public List<Worker> findAll(){
+		return rep.findAll();
+	}
+
+}
