a23113f8e0dad2c24123a065303de308e2c2b416
Emanuel Castro <emanuelsmcastro3@gmail.com>
2023-10-29 17:59:43 -0300

    create dtos - hr-oauth

---
+++ hr-oauth/src/main/java/com/microservice/hroauth/model/dtos/AuthRequest.java
@@ -0,0 +1,4 @@
+package com.microservice.hroauth.model.dtos;
+
+public record AuthRequest(String username, String password){
+}
---
+++ hr-oauth/src/main/java/com/microservice/hroauth/model/dtos/TokenDTO.java
@@ -0,0 +1,5 @@
+package com.microservice.hroauth.model.dtos;
+
+public record TokenDTO(String accessToken) {
+
+}
