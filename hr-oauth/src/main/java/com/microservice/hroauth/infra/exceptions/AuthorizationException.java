6a4d83ef28df9971b942fd693a269d7ac55a02c5
Emanuel Castro <emanuelsmcastro3@gmail.com>
2023-10-29 19:52:35 -0300

    Create AuthorizationException.java

---
+++ hr-oauth/src/main/java/com/microservice/hroauth/infra/exceptions/AuthorizationException.java
@@ -0,0 +1,11 @@
+package com.microservice.hroauth.infra.exceptions;
+
+public class AuthorizationException extends RuntimeException{
+
+	private static final long serialVersionUID = 1L;
+	
+	public AuthorizationException(String msg) {
+		super(msg);
+	}
+
+}
