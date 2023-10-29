8a5e7bc241fad79096b122f6f4e734db33ebe1dd
Emanuel Castro <emanuelsmcastro3@gmail.com>
2023-10-29 12:32:20 -0300

    populating database - hr-worker

---
+++ hr-worker/src/main/resources/import.sql
@@ -0,0 +1,3 @@
+INSERT INTO tb_worker(name, daily_income) VALUES ('Carl', 200.0);
+INSERT INTO tb_worker(name, daily_income) VALUES ('Blue', 100.0);
+INSERT INTO tb_worker(name, daily_income) VALUES ('Dav', 100.0);
