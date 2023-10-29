79adfd3d7fda73a6e2729b222ba5561187864e0c
Emanuel Castro <emanuelsmcastro3@gmail.com>
2023-10-29 18:30:33 -0300

    create entities - hr-oauth

---
+++ hr-oauth/src/main/java/com/microservice/hroauth/model/entities/User.java
@@ -0,0 +1,42 @@
+package com.microservice.hroauth.model.entities;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import org.hibernate.annotations.ManyToAny;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "tb_user")
+public class User {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	@Column(nullable = false)
+	private String username;
+	@Column(nullable = false)
+	private String password;
+	@ManyToAny(fetch = FetchType.EAGER)
+	@JoinTable(name = "tb_user_role", joinColumns = @JoinColumn(name = "user_id"), inverseJoinColumns = @JoinColumn(name = "role_id"))
+	private Set<Role> roles = new HashSet<>();
+
+	public boolean isAuthorized(Role role) {
+		return roles.contains(role);
+	}
+}
