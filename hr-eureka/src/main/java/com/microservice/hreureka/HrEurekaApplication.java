f35dc8ea3372329b979b556cac0aa8f582e0c4e9
Emanuel Castro <emanuelsmcastro3@gmail.com>
2023-10-29 13:22:21 -0300

    enabling eureka server - hr-eureka

--- hr-eureka/src/main/java/com/microservice/hreureka/HrEurekaApplication.java
+++ hr-eureka/src/main/java/com/microservice/hreureka/HrEurekaApplication.java
@@ -2,8 +2,10 @@ package com.microservice.hreureka;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class HrEurekaApplication {
 
 	public static void main(String[] args) {
