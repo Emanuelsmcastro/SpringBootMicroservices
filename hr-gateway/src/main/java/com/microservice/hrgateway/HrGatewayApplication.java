b62b28998850910aeff7303ececd19e3712f762a
Emanuel Castro <emanuelsmcastro3@gmail.com>
2023-10-29 16:58:10 -0300

    create gateway - hr-gateway

---
+++ hr-gateway/src/main/java/com/microservice/hrgateway/HrGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.microservice.hrgateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HrGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HrGatewayApplication.class, args);
+	}
+
+}
