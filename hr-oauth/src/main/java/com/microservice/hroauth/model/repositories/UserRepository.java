04633c9e08b752206fb963a37fb0d11c9f6656f2
Emanuel Castro <emanuelsmcastro3@gmail.com>
2023-10-29 19:53:28 -0300

    Add option user return - hr-oauth

--- hr-oauth/src/main/java/com/microservice/hroauth/model/repositories/UserRepository.java
+++ hr-oauth/src/main/java/com/microservice/hroauth/model/repositories/UserRepository.java
@@ -1,9 +1,12 @@
 package com.microservice.hroauth.model.repositories;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.microservice.hroauth.model.entities.User;
 
 public interface UserRepository extends JpaRepository<User, Long>{
 
+	Optional<User> findByUsername(String username);
 }
