8358197040f4be6f64ad7f21faba0cbee5a097a0
Emanuel Castro <emanuelsmcastro3@gmail.com>
2023-10-29 12:33:41 -0300

    create worker repository - hr-worker

---
+++ hr-worker/src/main/java/com/microservice/hrworker/model/repositories/WorkerRepository.java
@@ -0,0 +1,10 @@
+package com.microservice.hrworker.model.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.microservice.hrworker.model.entities.Worker;
+
+public interface WorkerRepository extends JpaRepository<Worker, Long>{
+	
+	public Worker findByName(String name);
+}
