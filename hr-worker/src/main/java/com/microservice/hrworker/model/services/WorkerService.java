88ef6e47a1427d69ba320878cc06a1f5218cf547
Emanuel Castro <emanuelsmcastro3@gmail.com>
2023-10-29 21:30:51 -0300

    service with token validation  - hr-worker

--- hr-worker/src/main/java/com/microservice/hrworker/model/services/WorkerService.java
+++ hr-worker/src/main/java/com/microservice/hrworker/model/services/WorkerService.java
@@ -1,11 +1,15 @@
 package com.microservice.hrworker.model.services;
 
+import java.util.Arrays;
 import java.util.List;
 import java.util.NoSuchElementException;
 
 import org.springframework.stereotype.Service;
 
+import com.microservice.hrworker.infra.exceptions.AuthorizationException;
 import com.microservice.hrworker.infra.exceptions.WorkerNotFoundException;
+import com.microservice.hrworker.model.dtos.UserResponse;
+import com.microservice.hrworker.model.entities.Role;
 import com.microservice.hrworker.model.entities.Worker;
 import com.microservice.hrworker.model.repositories.WorkerRepository;
 
@@ -15,9 +19,12 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class WorkerService {
 	
+	private static final List<String> allowedRoles = Arrays.asList("ROLE_OPERATOR", "ROLE_ADMIN");
 	private final WorkerRepository rep;
+	private final JwtService jwtService;
 	
-	public Worker findById(Long id) {
+	public Worker findById(Long id, String token) {
+		haveAuthorization(token);
 		try{
 			return rep.findById(id).get();
 		} catch(NoSuchElementException e) {
@@ -25,15 +32,27 @@ public class WorkerService {
 		}
 	}
 	
-	public Worker findByName(String name) {
+	public Worker findByName(String name, String token) {
+		haveAuthorization(token);
 		Worker worker = rep.findByNameIgnoreCase(name);
 		if(worker == null)
 			throw new WorkerNotFoundException(String.format("Worker (name:%s) not found.", name));
 		return worker;
 	}
 	
-	public List<Worker> findAll(){
+	public List<Worker> findAll(String token){
+		haveAuthorization(token);
 		return rep.findAll();
 	}
-
+	
+	private Boolean haveAuthorization(String token) {
+		UserResponse user = jwtService.getAuthenticatedUser(token);
+		for(Role role: user.roles()) {
+			for(String allowedRole: allowedRoles) {
+				if(role.getName().equals(allowedRole))
+					return true;
+			}
+		}
+		throw new AuthorizationException("You don't have permissions.");
+	}
 }
