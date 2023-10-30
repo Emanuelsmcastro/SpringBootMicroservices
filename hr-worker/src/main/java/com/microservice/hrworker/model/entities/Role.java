363ff0ce47a5b7d5d826acbd941ec82c8972c85e
Emanuel Castro <emanuelsmcastro3@gmail.com>
2023-10-29 21:30:05 -0300

    Role  - hr-worker

---
+++ hr-worker/src/main/java/com/microservice/hrworker/model/entities/Role.java
@@ -0,0 +1,25 @@
+package com.microservice.hrworker.model.entities;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "tb_role")
+public class Role {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	@Column(nullable = false)
+	private String name;
+}
