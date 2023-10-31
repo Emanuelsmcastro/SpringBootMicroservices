3760e6d5c217b814dd2c60da334ac43da7d102e1
Emanuel Castro <emanuelsmcastro3@gmail.com>
2023-10-30 23:01:12 -0300

    add exception treateament

--- autoBuild/runners/runnerBase.py
+++ autoBuild/runners/runnerBase.py
@@ -2,6 +2,7 @@ import asyncio
 import subprocess
 from pathlib import Path
 
+import aiofiles
 from utils.log import logger
 
 
@@ -9,7 +10,7 @@ class RunnerBase:
     
     DEBUG = True
     gradleBuildPattern = "**/build.gradle"
-    dockerFilePattern = "**/Dockerfile"
+    dockerFileName = "Dockerfile"
     
     gradleList = []
     gradleWarns = 0
@@ -18,12 +19,34 @@ class RunnerBase:
     
     dockerFileList = []
     
+    processes = []
+    
     def __init__(self, settings) -> None:
         self.settings: Path = settings
-    
+        
     def build(self):
-        asyncio.run(self.__buildGradle())
-    
+        loop = asyncio.get_event_loop()
+        try:
+            loop.run_until_complete(self.__buildGradle())
+            loop.run_until_complete(self.__updateDockerFile())
+        except Exception as e:
+            logger.exception(f'An error occurred: {e}')
+            self.__cleaningUp(loop)
+        except KeyboardInterrupt:
+            logger.warning("Received exit, exiting")
+            self.__cleaningUp(loop)
+        finally:
+            loop.close()
+            
+    def __cleaningUp(self, loop):
+        logger.info("Cleaning up")
+        pending_tasks = [
+            task for task in asyncio.all_tasks(loop) if not task.done()
+        ]
+        for process in self.processes:
+            process.terminate()
+        loop.run_until_complete(asyncio.gather(*pending_tasks))
+        
     def __findGradles(self):
         path: Path = self.settings.ROOT_DIR
         buildGradleList = path.glob(self.gradleBuildPattern)
@@ -32,7 +55,7 @@ class RunnerBase:
             self.gradleList.append(gradle)
 
     def __findDockerFile(self, gradle: Path):
-        dockerFile: Path = gradle.parent / Path('Dockerfile')
+        dockerFile: Path = gradle.parent / Path(self.dockerFileName)
         if dockerFile.exists():
             logger.info(f'Dockerfile found: {dockerFile}')
             self.gradleSuccess += 1
@@ -40,19 +63,37 @@ class RunnerBase:
         else:
             logger.warning(f'Dockerfile is missing: {dockerFile}')
             self.gradleWarns += 1
-    
+            
     def showInf(self, warns, erros):
         logger.warning(f'{"Warnins count":<14}: {warns:<3}')
         logger.error(f'{"Errors count":<14}: {erros:<3}')
         
+        
+    async def __openDockerFile(self, dockerFile: Path):
+        async with aiofiles.open(dockerFile, mode='r+') as file:
+            contents = await file.read()
+            logger.info(f'Found content: {contents}')
+            return dockerFile
+    
+    async def __updateDockerFile(self):
+        logger.info("Start update Dockerfile")
+        tasks = []
+        for _, dockerFile in enumerate(self.dockerFileList):
+            taskName = dockerFile.parent.name + " | " +  self.dockerFileName
+            task = asyncio.create_task(self.__openDockerFile(dockerFile), name=taskName)
+            tasks.append(task)
+            logger.info(f'Prepare task: {taskName}')
+        
+        for task in asyncio.as_completed(tasks):
+            result = await task
+            logger.info(f'Finished: {result}')
     
     async def __buildGradle(self):
         tasks = []
         self.__findGradles()
-        loop = asyncio.get_event_loop()
         for _, gradle in enumerate(self.gradleList):
             taskName = gradle.parent.name
-            task = asyncio.create_task(self.__getGradle(gradle, loop), name=taskName)
+            task = asyncio.create_task(self.__getGradle(gradle), name=taskName)
             tasks.append(task)
             logger.info(f'Prepare task: {taskName}')
             self.gradleSuccess += 1
@@ -65,7 +106,7 @@ class RunnerBase:
         self.showInf(self.gradleWarns, self.gradlErrors)
         logger.info(f'Finished gradle build !')
             
-    async def __getGradle(self, gradle, loop: asyncio.AbstractEventLoop,):
+    async def __getGradle(self, gradle):
         result = await self.__execBuildGradle(gradle)
         await asyncio.sleep(0)
         return result
@@ -77,6 +118,7 @@ class RunnerBase:
             command, stdout=subprocess.PIPE,
             stderr=subprocess.PIPE
         )
+        self.processes.append(process)
         if not self.DEBUG:
             stdout, stderr = await process.communicate()
             if stdout:
