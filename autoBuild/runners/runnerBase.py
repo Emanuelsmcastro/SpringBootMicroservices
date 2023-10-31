e631ece4a3a65c628155a7856d1de68ad49e630d
Emanuel Castro <emanuelsmcastro3@gmail.com>
2023-10-30 21:54:36 -0300

    add info counts

--- autoBuild/runners/runnerBase.py
+++ autoBuild/runners/runnerBase.py
@@ -9,7 +9,14 @@ class RunnerBase:
     
     DEBUG = True
     gradleBuildPattern = "**/build.gradle"
+    dockerFilePattern = "**/Dockerfile"
+    
     gradleList = []
+    gradleWarns = 0
+    gradlErrors = 0
+    gradleSuccess = 0
+    
+    dockerFileList = []
     
     def __init__(self, settings) -> None:
         self.settings: Path = settings
@@ -23,6 +30,21 @@ class RunnerBase:
         for gradle in buildGradleList:
             logger.info(f'Gradle found: {gradle}')
             self.gradleList.append(gradle)
+
+    def __findDockerFile(self, gradle: Path):
+        dockerFile: Path = gradle.parent / Path('Dockerfile')
+        if dockerFile.exists():
+            logger.info(f'Dockerfile found: {dockerFile}')
+            self.gradleSuccess += 1
+            self.dockerFileList.append(dockerFile)
+        else:
+            logger.warning(f'Dockerfile is missing: {dockerFile}')
+            self.gradleWarns += 1
+    
+    def showInf(self, warns, erros):
+        logger.warning(f'{"Warnins count":<14}: {warns:<3}')
+        logger.error(f'{"Errors count":<14}: {erros:<3}')
+        
     
     async def __buildGradle(self):
         tasks = []
@@ -33,10 +55,15 @@ class RunnerBase:
             task = asyncio.create_task(self.__getGradle(gradle, loop), name=taskName)
             tasks.append(task)
             logger.info(f'Prepare task: {taskName}')
+            self.gradleSuccess += 1
         
         for task in asyncio.as_completed(tasks):
             result = await task
             logger.info(f'Finished: {result}')
+            self.gradleSuccess += 1
+            self.__findDockerFile(result)
+        self.showInf(self.gradleWarns, self.gradlErrors)
+        logger.info(f'Finished gradle build !')
             
     async def __getGradle(self, gradle, loop: asyncio.AbstractEventLoop,):
         result = await self.__execBuildGradle(gradle)
@@ -54,8 +81,10 @@ class RunnerBase:
             stdout, stderr = await process.communicate()
             if stdout:
                 logger.info(f'[stdout]\n{stdout.decode()}')
+                self.gradleSuccess += 1
             if stderr:
                 logger.error(f'[stderr]\n{stderr.decode()}')
+                self.gradlErrors += 1
         else:
             while True:
                 output = await process.stdout.readline()
